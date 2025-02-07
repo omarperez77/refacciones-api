69dcecf8f19af70999c5adecf3ad6245c9c8a6e8
Omar Gerardo Pérez Morales <omar.perez@lahuerta.tecmm.edu.mx>
2025-02-07 13:03:14 -0600

    Configurando para deplyment en render

---
+++ server.js
@@ -0,0 +1,14 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+// Middleware to parse JSON request bodies
+const PORT = process.env.PORT || 3001;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(PORT, () => {
+    console.log(`JSON Server esta corriendo en el puerto ${PORT}`);
+});
